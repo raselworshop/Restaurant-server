b13b54b6104d7dba3014912d566703b5eda0b53d
raselworshop <raselworshop@gmail.com>
2025-01-02 16:07:18 +0800

    added the delete method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -49,6 +49,12 @@ async function run() {
     app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartsCollection.insertOne(cartItem)
+      res.send(result)
+    })
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query= {_id: new ObjectId(id)}
+      const result= await cartsCollection.deleteOne(query)
       res.send(result)
     })
 
