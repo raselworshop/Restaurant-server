3c3fb9d03806a8a5a78f6fa85cf72c76ea380eee
raselworshop <raselworshop@gmail.com>
2025-01-07 00:04:37 +0800

    added the menu post by admins

--- index.js
+++ index.js
@@ -116,6 +116,11 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result)
     })
+    app.post('/menu', tokenVerify, verifyAdmin,  async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
+    })
     // reviews related 
     app.get('/reviews', async (req, res) => {
       const result = await reviewsCollection.find().toArray();
